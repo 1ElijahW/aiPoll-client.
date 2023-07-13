0a44e6ccde0de8b91f46ec257d7b8a0cbe09feee
Alison <alison.bayona@gmail.com>
2023-07-13 14:39:28 -0400

    style/codeadjustment

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,62 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import Comment from '../components/Comment';
-import base from '../auth/baseURL';
+// import React, { useEffect, useState } from 'react';
+// import Comment from '../components/Comment';
+// import base from '../auth/baseURL';
 
-function Chatbox(chatbox_id) {
-  const [chatbox, setChatbox] = useState([]);
+// function Chatbox(chatbox_id) {
+//   const [chatbox, setChatbox] = useState([]);
 
-  useEffect(() => {
-    fetchChatbox();
-  }, []);
-
-
-  const fetchChatbox = async () => {
-    try {
-      const response = await base.get(`/api/chatboxes/${chatbox_id}/`, {});
-      setComments(response.data);
-    } catch (error) { console.error(error) }
-  };
-
-  useEffect(() => {
-    // Fetch polls on component mount
-    fetchComments();
-  }, []);
+//   useEffect(() => {
+//     fetchChatbox();
+//   }, []);
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add your logic to handle submitting a comment
-    // You can store the comment, user handle, profile picture, and timestamp in the comments state
-  };
+//   const fetchChatbox = async () => {
+//     try {
+//       const response = await base.get(`/api/chatboxes/${chatbox_id}/`, {});
+//       setComments(response.data);
+//     } catch (error) { console.error(error) }
+//   };
 
-  return (
-        <div className="chatbox">
-          <div className="chatbox-header">
-            <h3>Chatbox</h3>
-          </div>
-          <div className="chatbox-comments">
-          {chatbox.map((chatbox) => (
-  <div key={chatbox.id} className="comment">
-    <div className="user-profile">
-      <span>{chatbox.creator_name}</span>
-    </div>
-    <div className="chatbox-content">
-      <p>{chatbox.text}</p>
-      <span>{chatbox.created_at}</span>
-    </div>
-  </div>
-))}
+//   useEffect(() => {
+//     // Fetch polls on component mount
+//     fetchComments();
+//   }, []);
 
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <textarea required></textarea>
-            </div>
-            <button type="submit">Send</button>
-          </form>
-        </div>
-      );
-}
 
-export default Chatbox;
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Add your logic to handle submitting a comment
+//     // You can store the comment, user handle, profile picture, and timestamp in the comments state
+//   };
+
+//   return (
+//         <div className="chatbox">
+//           <div className="chatbox-header">
+//             <h3>Chatbox</h3>
+//           </div>
+//           <div className="chatbox-comments">
+//           {chatbox.map((chatbox) => (
+//   <div key={chatbox.id} className="comment">
+//     <div className="user-profile">
+//       <span>{chatbox.creator_name}</span>
+//     </div>
+//     <div className="chatbox-content">
+//       <p>{chatbox.text}</p>
+//       <span>{chatbox.created_at}</span>
+//     </div>
+//   </div>
+// ))}
+
+//           </div>
+//           <form onSubmit={handleSubmit}>
+//             <div>
+//               <textarea required></textarea>
+//             </div>
+//             <button type="submit">Send</button>
+//           </form>
+//         </div>
+//       );
+// }
+
+// export default Chatbox;
 
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './Comment.css';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import './Comment.css';
 
-const Comment = ({comment}) => {
-    id: 1,
-    creator_name: "user1",
-    text: "testing",
-    created_at: "2023-07-12T17:51:00.091327Z",
-  chatbox: 13
-}
+// const Comment = ({comment}) => {
+//     id: 1,
+//     creator_name: "user1",
+//     text: "testing",
+//     created_at: "2023-07-12T17:51:00.091327Z",
+//   chatbox: 13
+// }
 
-function Comment(comments_id) {
-  const [comment, setComment] = useState([]);
+// function Comment(comments_id) {
+//   const [comment, setComment] = useState([]);
 
-  useEffect(() => {
-    fetchComment();
-  }, []);
+//   useEffect(() => {
+//     fetchComment();
+//   }, []);
 
-  const fetchComment = async () => {
-    try {
-      const response = await axios.get(`https://ai-poll-b30b8a89907a.herokuapp.com/api/comments/${comments_id}/`, {
-        headers: {
-          Authorization: 'token 0778a77e06eaa48a2be71a412a4c966c1a815000'
-        }
-      });
-      setComment(response.data);
-    } catch (error) { console.error(error) }
-  };
+//   const fetchComment = async () => {
+//     try {
+//       const response = await axios.get(`https://ai-poll-b30b8a89907a.herokuapp.com/api/comments/${comments_id}/`, {
+//         headers: {
+//           Authorization: 'token 0778a77e06eaa48a2be71a412a4c966c1a815000'
+//         }
+//       });
+//       setComment(response.data);
+//     } catch (error) { console.error(error) }
+//   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-   d
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//    d
+//   };
 
-  return (
-    <div className="comment-container">
-      {comment.map((comment) => (
-        <p key={comment.id}>{comment.text}</p>
-    </div>
-    ))};
-</div>
-<form onSubmit={handleSubmit}>
-  <div>
-    <textarea required></textarea>
-  </div>
-  <button type="submit">Send</button>
-</form>
-</div>
-);
-}
+//   return (
+//     <div className="comment-container">
+//       {comment.map((comment) => (
+//         <p key={comment.id}>{comment.text}</p>
+//     </div>
+//     ))};
+// </div>
+// <form onSubmit={handleSubmit}>
+//   <div>
+//     <textarea required></textarea>
+//   </div>
+//   <button type="submit">Send</button>
+// </form>
+// </div>
+// );
+// }
 
-export default Comment;
+// export default Comment;
